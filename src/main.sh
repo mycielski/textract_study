fd26019562f5b2a044911b7428682a9038962e66
Tomek Mycielski <tmyciels@Tomeks-MacBook-Pro.local>
2023-11-27 02:24:44 +0100

    shell script

--- src/main.sh
+++ src/main.sh
@@ -14,7 +14,8 @@ trap "rm -f /tmp/s3_objects" EXIT INT TERM HUP
 
 # shellcheck disable=SC2140
 # shellcheck disable=SC2016
-find "$1" -type f \( -name "*.png" -or -name "*.jpg" -or -name "*.jpeg" -or -name "*.pdf" \) -print0 |
+#find "$1" -type f \( -name "*.png" -or -name "*.jpg" -or -name "*.jpeg" -or -name "*.pdf" \) -print0 |
+find "$1" -type f \( -name "*.png" \) -print0 |
 xargs -0 -n 1 -P $workers -I {} aws s3 cp --no-progress "{}" s3://"$AWS_BUCKET"/"$job_uuid"/"{}" >> /tmp/s3_objects &
 
 awk -F  "s3://$AWS_BUCKET/" '{print $2}' /tmp/s3_objects |
@@ -24,11 +25,11 @@ pv --rate-limit 64 --quiet |
 xargs -n 1 -P $workers aws textract start-expense-analysis --document-location --region "$AWS_REGION" --no-cli-pager |
 jq -r '.JobId' > /tmp/job_ids &
 
-#mkfifo /tmp/responses
-#trap "rm -f /tmp/responses" EXIT INT TERM HUP
-#mkfifo /tmp/errors
-#trap "rm -f /tmp/errors" EXIT INT TERM HUP
-#
+mkfifo /tmp/responses
+trap "rm -f /tmp/responses" EXIT INT TERM HUP
+mkfifo /tmp/errors
+trap "rm -f /tmp/errors" EXIT INT TERM HUP
+
 #while read -r job_id; do
 #  echo "Job ID: $job_id"
 #  response=$(aws textract get-expense-analysis --job-id "$job_id" --region "$AWS_REGION" --no-cli-pager)
@@ -36,7 +37,7 @@ jq -r '.JobId' > /tmp/job_ids &
 #  if [ "$job_status" = "SUCCEEDED" ]; then
 #    echo "$response" >> /tmp/responses
 #  elif [ "$job_status" = "IN_PROGRESS" ]; then
-#    echo "$response" >> /tmp/job_ids
+#    echo "$job_id" >> /tmp/job_ids
 #  elif [ "$job_status" = "PARTIAL_SUCCESS" ]; then
 #    echo "$response" >> /tmp/responses
 #  else
